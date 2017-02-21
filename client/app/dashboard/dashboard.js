28f2bfaf1d7f17c631143386dc02071f0ad8d0db
Eric Forsell <efor202@gmail.com>
2017-02-20 19:24:45 -0500

    Integrate add goal into dashboard controller

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -2,14 +2,13 @@
   'use strict';
 
   angular
-    .module('dashboard', [])
+    .module('dashboard', ['ui.materialize'])
     .controller('DashboardCtrl', function($scope, $http, authService, jwtHelper, lock, GoalFactory) {
 
     var vm = this;
 
     lock.getProfile(localStorage.getItem('id_token'), function (error, profile) {
       vm.payload = profile;
-      console.log(profile);
       GoalFactory.findOrCreateUser(vm.payload.name, vm.payload.email)
         .then(user => {
           vm.user = user.data[0]
@@ -21,6 +20,16 @@
         });
     });
 
-    vm.test = () => console.log(vm.goals);
-  });
+    vm.addGoal = function() {//adds the entered goal into the database
+      GoalFactory.createGoal(vm.goal, vm.payload.email).then(function() {
+        GoalFactory.getUserGoals(vm.payload.email)//reloads the goals dynamically
+          .then(goals => {
+            vm.goals = goals.data;
+          });
+      });
+    vm.goal = '';
+  }
+
+});
 }());
+
